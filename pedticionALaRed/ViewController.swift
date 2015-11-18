334b50007e8a5a61e664d96cd7159434fad59839
Edwin DAnilo Adriano Gallardo <adn-777@hotmail.com>
2015-11-18 15:05:04 +0100

    prueba github

--- pedticionALaRed/ViewController.swift
+++ pedticionALaRed/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //prueba github
 
+    @IBOutlet weak var tftexto: UITextField!
+    @IBOutlet weak var tvPantalla: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func sincrono(){
+        let isbt = tftexto.text
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbt!)"
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)//agregamos "!" al url ya que al modificar la direccion se nos hace opcional
+        if datos == nil{
+            let alerta = UIAlertController(title: "Alerta", message: "No dispone de servicio de internet", preferredStyle: .Alert)
+            alerta.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alerta, animated: true, completion: nil)
+        }else{
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            tvPantalla.text = String(texto)
+        }
+    }
 
-
+    @IBAction func serchTextField(sender: UITextField) {
+        if tftexto.text == "" {
+            let alerta = UIAlertController(title: "Alerta", message: "Ingrese un numero", preferredStyle: .Alert)
+            alerta.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alerta, animated: true, completion: nil)
+        } else{
+            sincrono()
+            sender.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func deseparecerTeclado(sender: AnyObject) {
+        tftexto.resignFirstResponder()
+        tvPantalla.resignFirstResponder()
+    }
+    
+    @IBAction func limpiar(sender: UIButton) {
+        tftexto.text = String()
+        tvPantalla.text = String()
+    }
 }
+
+
+
+
+
+
+
+
+
 
